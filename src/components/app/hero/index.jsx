d134b3c4e0f5b490931d56a0995542303cdc9d06
alimpp <hmdmpp@gmail.com>
2026-05-09 19:22:29 +0330

    Create hero

--- src/components/app/hero/index.jsx
+++ src/components/app/hero/index.jsx
@@ -1,10 +1,30 @@
+import StyleModule from "./styles/index.module.css";
+import BaseButton from "../../base/button/index";
+
 const Hero = () => {
   return (
-    <div className="container h-full flex align-center">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure eligendi
-      beatae, voluptate amet quod esse corrupti odio id debitis quaerat
-      architecto fugit praesentium laudantium obcaecati? Accusamus iure quis
-      doloribus ullam.
+    <div className={`h-full flex align-center ${StyleModule.heroContent}`}>
+      <div className={`${StyleModule.glass}`}>
+        <div className={`container flex flex-column`}>
+          <span className="text-primary">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit
+          </span>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum,
+            repudiandae ullam doloremque aliquid dignissimos iure culpa modi
+            eligendi incidunt quasi enim assumenda harum nulla mollitia optio?
+            Quae doloribus eligendi magni.
+          </p>
+          <div className="flex">
+            <div>
+              <BaseButton>Get Started</BaseButton>
+            </div>
+            <div className="mx-10">
+              <BaseButton bg="bgRed">About Us</BaseButton>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
